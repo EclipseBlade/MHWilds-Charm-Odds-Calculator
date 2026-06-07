6f8f990fdd0c1bc397b08ed886ab93659b897127
Eclipse <EclipsesBlade@gmail.com>
2026-06-07 18:23:59 -0400

    fix linting issues

--- src/store/mhwStore.js
+++ src/store/mhwStore.js
@@ -1,7 +1,6 @@
 import { create } from "zustand"
 import { persist } from "zustand/middleware"
 import { computeCharmProb } from "../lib/amuletProb"
-import rarityBaseProbability from "../data/Rarity.json"
 
 const useMhwStore = create(
   persist(
